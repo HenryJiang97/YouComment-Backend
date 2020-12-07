12363c2db3a8df0a146f67c80d311eb522a26051
Henry Jiang <henryjiang97@gmail.com>
2020-12-07 14:15:06 -0800

    Added user APIs

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
 const express = require('express');
 const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const urlRouter = require('./routes/url');
-const bodyParser = require('body-parser');
+const userRouter = require('./router/user');
+const commentRouter = require('./router/comment');
 const mongoose = require('mongoose');
-const { resourceLimits } = require('worker_threads');
 
 const app = express();
-
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
+// Connect to MongoDB
 const mongoDBEndpoint = "mongodb+srv://HenryJiang:hanhuajiang@cluster0.mhawq.mongodb.net/youcomment?retryWrites=true&w=majority";
 mongoose.connect(mongoDBEndpoint, { useNewUrlParser: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Error connecting to mongo db'));
 
+// Use API
 app.use(cors());
-app.use('/api/url', urlRouter);
+app.use('/api/user', userRouter);
+app.use('/api/comment', commentRouter);
 
+// Port setup
 const port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("starting at" + port);
